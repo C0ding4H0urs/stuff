65953d0b40fbd332e4851a9dcfebd214ab9fc2ff
Russell2259 <84256826+Russell2259@users.noreply.github.com>
2023-01-27 11:16:42 -0700

    test

--- devtools.js
+++ devtools.js
@@ -1,12 +1,12 @@
-const frame = document.createElement('iframe');
-frame.setAttribute('src','');
-frame.setAttribute('width', 500);
-frame.setAttribute('height', 500);
-document.body.appendChild(frame);
+const frameEl = document.createElement('iframe');
+frameEl.setAttribute('src','');
+frameEl.setAttribute('width', 500);
+frameEl.setAttribute('height', 500);
+document.body.appendChild(frameEl);
 
 const fetch = await fetch('https://russell2259.github.io/stuff/devtools.html');
 const template = fetch.json();
+const frame = frameEl.contentDocument;
 
-frame.contentWindow.document.write(template);
-
-alert('...');
+frame.documentElement = template;
+frame.eval('alert("test")');
