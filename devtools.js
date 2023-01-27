e271ddb886ff58f31535ea297d15d1e5edcaa0c3
Russell2259 <84256826+Russell2259@users.noreply.github.com>
2023-01-27 10:24:32 -0700

    update

--- devtools.js
+++ devtools.js
@@ -1,5 +1,7 @@
 var frame = document.createElement('iframe');
 frame.setAttribute('src','');
+frame.setAttribute('width', 500);
+frame.setAttribute('height', 500);
 document.body.appendChild(frame);
 
 frame.contentWindow.document.documentElement = `Hello ${prompt('What is your name?')}!`;
--- devtoolsbookmarklet.js
+++ devtoolsbookmarklet.js
@@ -1,1 +1,1 @@
-javascript:var devtools = document.createElement('script');devtools.setAttribute('src','https://raw.githubusercontent.com/Russell2259/stuff/main/devtools.js');document.body.appendChild(devtools);
+javascript:var devtools = document.createElement('script');devtools.setAttribute('src','https://russell2259.github.io/stuff/devtools.js');document.body.appendChild(devtools);
