e17492ab1811ba4ee034bc38672d83d723507dba
Russell2259 <84256826+Russell2259@users.noreply.github.com>
2023-01-27 10:57:46 -0700

    test

--- devtools.js
+++ devtools.js
@@ -4,4 +4,7 @@ frame.setAttribute('width', 500);
 frame.setAttribute('height', 500);
 document.body.appendChild(frame);
 
-frame.contentDocument.write(`Hello ${prompt('What is your name?')}!`);
+const fetch = await fetch('https://russell2259.github.io/stuff/devtools.html');
+const template = fetch.json();
+
+frame.contentWindow.document.write(template);
