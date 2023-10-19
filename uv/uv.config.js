6ab51e4fc1bfeb2d23ada258bab6c58b473c26e0
Russell <russell2259@tuta.io>
2023-10-18 18:42:36 -0600

    Create uv.config.js

---
+++ uv/uv.config.js
@@ -0,0 +1,11 @@
+self.__uv$config = {
+    prefix: '/stuff/uv/service/',
+    bare: 'https://tomp.app/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: 'https://unpkg.com/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.handler.js',
+    client: 'https://unpkg.com/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.client.js',
+    bundle: 'https://unpkg.com/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.bundle.js',
+    config: 'https://unpkg.com/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.config.js',
+    sw: 'https://unpkg.com/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.sw.js',
+};
