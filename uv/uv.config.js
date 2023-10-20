1fe7e1dd4f92070f0068451c89400222efbeffd7
Russell <russell2259@tuta.io>
2023-10-19 18:24:35 -0600

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,11 +1,11 @@
 self.__uv$config = {
     prefix: '/stuff/uv/service/',
-    bare: 'https://polarislearning.arg/bare/',
+    bare: 'https://polarislearning.art/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: 'https://unpkg.com/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.handler.js',
-    client: 'https://unpkg.com/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.client.js',
-    bundle: 'https://unpkg.com/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.bundle.js',
-    config: 'https://unpkg.com/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.config.js',
-    sw: 'https://unpkg.com/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.sw.js',
+    handler: './uv.handler.js',
+    client: './uv.client.js',
+    bundle: './uv.bundle.js',
+    config: './uv.config.js',
+    sw: './uv.sw.js',
 };
